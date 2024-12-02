fda25e456bfa088a656bc1217b98fa82a0008f87
Praveen S <praveens321123@gmail.com>
2024-12-02 18:37:01 +0530

    Indentations

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,10 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://34c142b1d81e8432b582d97d4d579eb7@o4508398632370176.ingest.us.sentry.io/4508398658650112",
 
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-  
 });
